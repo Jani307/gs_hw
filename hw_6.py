89036b66e68fb1e0db389c9a6243e15b9940d502
Ivan Hutsuliak <i.i.hutsuliak@gmail.com>
2024-12-07 21:57:03 +0200

    HW 6 is finished

---
+++ hw_6.py
@@ -0,0 +1,61 @@
+"""
+**Завдання 1: Створення класу і об'єктів**
+
+Створіть клас `Animal`, який представляє тварину. Кожний об'єкт класу `Animal` повинен мати наступні атрибути:
+
+- `name` (ім'я тварини)
+- `species` (вид тварини)
+- `age` (вік тварини)
+
+Створіть конструктор класу, який ініціалізує ці атрибути при створенні об'єкта. Напишіть метод `make_sound()`, який буде виводити звук, який виділяє тварина.
+
+Створіть два об'єкта класу `Animal` з різними характеристиками та викличте їхні методи `make_sound()`.
+
+**Завдання 2: Робота з об'єктами**
+
+Створіть клас `Rectangle`, який представляє прямокутник. Кожен об'єкт класу `Rectangle` повинен мати наступні атрибути:
+
+- `width` (ширина прямокутника)
+- `height` (висота прямокутника)
+
+Створіть конструктор класу, який ініціалізує ці атрибути при створенні об'єкта. Напишіть метод `calculate_area()`, який розраховує площу прямокутника (площа = ширина * висота).
+
+Створіть два об'єкта класу `Rectangle` з різними розмірами та викличте їхні методи `calculate_area()`, виведіть площу прямокутників на екран.
+"""
+
+
+# Створення класу і об'єктів
+class Animal:
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age
+
+    def make_sound(self):
+        if self.age > 10:
+            print(f'Old {self.species} {self.name} sounds small.')
+        else:
+            print(f'Young {self.species} {self.name} sounds loud.')
+
+
+dog = Animal('Spark', 'dog', 12)
+cat = Animal('Wiskas', 'cat', 3)
+
+dog.make_sound()
+cat.make_sound()
+
+
+# Робота з об'єктами
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.height * self.width
+
+
+rec_1 = Rectangle(2, 4)
+rec_2 = Rectangle(120, 65)
+
+print(rec_1.calculate_area(), rec_2.calculate_area(), sep='\n')
